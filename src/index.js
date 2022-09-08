7bf24a21aa6e0028dacd332c0d5b418d66982053
RCurvo <renan.curvo@gmail.com>
2022-09-08 11:13:39 -0300

    Implements bank statement by date

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 const { response, request } = require('express');
 const express = require('express');
-const {v4: uuidv4} = require("uuid")
+const { v4: uuidv4 } = require("uuid")
 
 const app = express();
 
 const customers = [];
 
 function verifyIfExistsAccountCPF(request, response, next) {
-    const {cpf} = request.headers;
+    const { cpf } = request.headers;
 
     const customer = customers.find((customer) => customer.cpf === cpf)
 
-    if(!customer){
-        return response.status(400).json({error: "customer not found"})
+    if (!customer) {
+        return response.status(400).json({ error: "customer not found" })
     }
     request.customer = customer
 
     return next()
 }
 
-function getBalance(statement){
+function getBalance(statement) {
     const balance = statement.reduce((acc, operation) => {
-       if(operation.type == 'creddit'){
-        return acc + operation.amount;
-       } else {
-        return acc = operation.amount
-       }
+        if (operation.type == 'creddit') {
+            return acc + operation.amount;
+        } else {
+            return acc = operation.amount
+        }
     }, 0)
 
     return balance;
@@ -34,11 +34,11 @@ function getBalance(statement){
 app.use(express.json())
 
 app.post("/account", (request, response) => {
-    const {cpf, name} = request.body;
+    const { cpf, name } = request.body;
     const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
 
-    if(customerAlreadyExists){
-        return response.status(400).json({error: "Customer already exists!"})
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" })
     }
     customers.push({
         cpf,
@@ -50,24 +50,33 @@ app.post("/account", (request, response) => {
 })
 
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
-const {customer} = request;
-return response.json(customer.statement)
+    const { customer } = request;
+    return response.json(customer.statement)
 })
 
-app.post("/deposit", verifyIfExistsAccountCPF, (request, response) =>{
-const { description, amount} = request.body
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+    const dateFormat = new Date(date + " 00:00");
+    const statement = customer.statement.filter(
+        (statement) => statement.created_at.toDateString() == new Date(dateFormat).toDateString())
+    return response.json(statement)
+})
 
-const {customer} = request;
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body
 
-const statementOperation = {
-    description,
-    amount,
-    created_at: new Date(),
-    type: "credit"
-}
-customer.statement.push(statementOperation);
+    const { customer } = request;
 
-return response.status(201).send("Successful deposit")
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send("Successful deposit")
 })
 
 app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
@@ -75,17 +84,17 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
 
     const { customer } = request;
     const balance = getBalance(customer.statement);
-    if(balance < amount){
-        return response.status(400).json({error: "Insufficient funds!"})
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" })
     }
     const statementOperation = {
         amount,
         created_at: new Date(),
         type: "debit",
-     }
-     customer.statement.push(statementOperation);
+    }
+    customer.statement.push(statementOperation);
 
-     return response.status(201).send("Succesfull Withdraw");
+    return response.status(201).send("Succesfull Withdraw");
 })
 
 app.listen(3333);
